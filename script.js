fad43f660f7a97ba8dcfa3c6cac16e0c6d672321
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-06-25 20:27:06 +0300

    adding testing grid

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let abody = document.querySelector("body")
 let testdiv = document.querySelector(".testlist")
-
+counter = 0
+let theButtons = document.querySelectorAll(".inline")
 
 
 function generateNumber(){
-    const randomNmuber = Math.floor(Math.random() * 5)
+    counter+=1        
+    const randomNmuber = counter
     const listItem = document.createElement("div");
     listItem.className = "inline";
     const textnode = document.createTextNode(randomNmuber)
     const rando = listItem.appendChild(textnode);
     document.querySelector(".container").appendChild(listItem);
-
-
 }
+for (let c = 0; c <= 64; c++){
+generateNumber()
+}
+
+theButtons.forEach(button => (button.addEventListener("click" , e =>{
+    console.log("hi")
+})))
+console.log(theButtons)
 
 
 
