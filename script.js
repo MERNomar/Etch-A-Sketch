a5ca7712a24d03eec40d589c5dba7c82e4f60d96
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-06-27 14:08:37 +0300

    remaking my whole code(css , js)
    learnd grid and now i can work wtih it more effectivly and control my project in a better way
    added the rebeat function to my css and controled it with my javascript
    now my function will multiply 16*16 and 32 * 32 and will create better board

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const container = document.getElementById("container");
 
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
+  container.style.gridTemplateRows = `repeat(${rows}), auto)`;
+  container.style.gridTemplateColumns = `repeat(${cols}, auto)`;
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16, 16);
+makeRows(32, 32);
