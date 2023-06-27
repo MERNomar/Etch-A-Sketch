d4e6f20d6a364a150bbc38a1ea38edcff95f06ec
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-06-27 17:11:20 +0300

    finshing the project (will be back later)

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const container = document.getElementById("container");
+const gridItem = document.querySelector(".grid-item");
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
+let stop = false
 
 function makeRows(rows, cols) {
+  if (stop === true)   
+  stop = true
   container.style.gridTemplateRows = `repeat(${rows}), auto)`;
   container.style.gridTemplateColumns = `repeat(${cols}, auto)`;
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
+    cell.addEventListener('mouseover' , e => {cell.style.backgroundColor = `#${randomColor}`})
+    document.getElementById('clear').addEventListener('click' , e => {cell.style.backgroundColor = 'white'})
+
   };
 };
-
-makeRows(32, 32);
+console.log(randomColor)
