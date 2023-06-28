d53777047ac74f0e471f44ef8778fd6fcffb2b7a
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-06-28 13:56:10 +0300

    updating the rainbow mode

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const container = document.getElementById("container");
 const gridItem = document.querySelector(".grid-item");
-let stop = false
-
+var stop = false
 function makeRows(rows, cols) {
   if (stop === true)   
   stop = true
   container.style.gridTemplateRows = `repeat(${rows}), auto)`;
   container.style.gridTemplateColumns = `repeat(${cols}, auto)`;
   for (c = 0; c < (rows * cols); c++) {
-    let rainbow = true
+    let rainbow = false
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
     if (rainbow === false){
-      cell.addEventListener('mouseover' , e => {cell.style.backgroundColor = `#${randomColor}`})
+      cell.addEventListener('mousedown' , e => {cell.style.backgroundColor = `#${randomColor}`})
     }else{
-      cell.addEventListener('mouseover' , e => {cell.style.backgroundColor = `black`})
+      cell.addEventListener('mousedown' , e => {cell.style.backgroundColor = `black`})
     }
     document.getElementById('clear').addEventListener('click' , e => {rainbow = false})
 
