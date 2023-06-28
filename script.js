577c38708cfc0daefd9f6f234f04efd0013401bf
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-06-28 07:55:50 +0300

    adding rainbow (still working on it)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const container = document.getElementById("container");
 const gridItem = document.querySelector(".grid-item");
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
 let stop = false
 
 function makeRows(rows, cols) {
@@ -9,11 +8,16 @@ function makeRows(rows, cols) {
   container.style.gridTemplateRows = `repeat(${rows}), auto)`;
   container.style.gridTemplateColumns = `repeat(${cols}, auto)`;
   for (c = 0; c < (rows * cols); c++) {
+    let rainbow = true
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-    cell.addEventListener('mouseover' , e => {cell.style.backgroundColor = `#${randomColor}`})
-    document.getElementById('clear').addEventListener('click' , e => {cell.style.backgroundColor = 'white'})
+    if (rainbow === false){
+      cell.addEventListener('mouseover' , e => {cell.style.backgroundColor = `#${randomColor}`})
+    }else{
+      cell.addEventListener('mouseover' , e => {cell.style.backgroundColor = `black`})
+    }
+    document.getElementById('clear').addEventListener('click' , e => {rainbow = false})
 
   };
 };
-console.log(randomColor)
